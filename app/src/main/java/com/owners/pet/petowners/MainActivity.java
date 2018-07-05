3f6b07d63329c588e37f740afccf73c2ed5aed11
didem <kucukkaraaslan.didem@gmail.com>
2018-07-05 12:40:08 +0300

    Task 1 Complete

--- app/src/main/java/com/owners/pet/petowners/MainActivity.java
+++ app/src/main/java/com/owners/pet/petowners/MainActivity.java
@@ -13,85 +13,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.GeoDataClient;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
-
 public class MainActivity extends AppCompatActivity {
-    private GeoDataClient mGeoDataClient;
-    int ACCESS_FINE_LOCATION_PERMISSION_REQUEST = 1;
-    int PLACE_PICKER_REQUEST = 11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)){
-
-                Toast.makeText(this, "We need this permission to know your current place",
-                        Toast.LENGTH_LONG).show();
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        ACCESS_FINE_LOCATION_PERMISSION_REQUEST);
-
-            }else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        ACCESS_FINE_LOCATION_PERMISSION_REQUEST);
-            }
-        } else {
-            mGeoDataClient = Places.getGeoDataClient(this);
-            pickPlace();
-        }
-
-
-
-
     }
 
-    private void pickPlace() {
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
-        try {
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST );
-        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 1:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission granted yay!",
-                            Toast.LENGTH_LONG).show();
-                    pickPlace();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Permission denied fuck!",
-                            Toast.LENGTH_LONG).show();
-                }
-                return;
-
-        }
-    }
 }
