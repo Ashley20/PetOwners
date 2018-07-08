885ddef435cf8dcd78c4908da3dad64a8d1eb6f7
didem <kucukkaraaslan.didem@gmail.com>
2018-07-08 14:25:44 +0300

    Profile image selection

--- app/src/main/java/com/owners/pet/petowners/ProfileActivity.java
+++ app/src/main/java/com/owners/pet/petowners/ProfileActivity.java
@@ -4,19 +4,31 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,11 +36,14 @@ import butterknife.OnClick;
 
 public class ProfileActivity extends AppCompatActivity {
     private static final int PICK_IMAGE = 1;
+    private static final String FIREBASE_STORAGE_IMAGES_REFERENCE_URL = "gs://petowners.appspot.com";
     @BindView(R.id.profile_picture_image_view) ImageView profile_picture;
     @BindView(R.id.phone_text_view) TextView phone;
     @BindView(R.id.email_text_view) TextView email;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +60,28 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
         mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
     }
 
     @Override
     protected void onStart() {
         currentUser = mAuth.getCurrentUser();
+        // If the user is not null then update the UI with current user's profile information
         if (currentUser != null) {
             profile_picture.setImageURI(currentUser.getPhotoUrl());
         }
         super.onStart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @OnClick(R.id.profile_picture_image_view)
@@ -68,23 +96,42 @@ public class ProfileActivity extends AppCompatActivity {
         switch(requestCode) {
             case PICK_IMAGE:
                 if(resultCode == RESULT_OK){
-                    try {
-                        final Uri imageUri = data.getData();
-                        InputStream imageStream = null;
-                        if (imageUri != null) {
-                            imageStream = getContentResolver().openInputStream(imageUri);
+                    final Uri imageUri = data.getData();
+
+                    // Update user photo
+                    final UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setPhotoUri(imageUri)
+                            .build();
+
+                    currentUser.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            profile_picture.setImageURI(currentUser.getPhotoUrl());
                         }
-                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                        profile_picture.setImageBitmap(selectedImage);
-
-
-
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+                    });
 
                 }
         }
+    }
+
+    private void uploadImageToFirebaseStorage(InputStream imageStream) {
+        storageRef = storage.getReference();
+        StorageReference profileImagesRef = storageRef.child("profile.jpg");
+
+        UploadTask uploadTask = profileImagesRef.putStream(imageStream);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
+
     }
 
 
