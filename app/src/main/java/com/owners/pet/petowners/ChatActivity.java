5992f33cda38384d382e56dc9987d1d0f6d763e1
didem <kucukkaraaslan.didem@gmail.com>
2018-07-16 13:38:47 +0300

    custom chat actionbar

--- app/src/main/java/com/owners/pet/petowners/ChatActivity.java
+++ app/src/main/java/com/owners/pet/petowners/ChatActivity.java
@@ -1,46 +1,75 @@
 package com.owners.pet.petowners;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.owners.pet.petowners.Glide.GlideApp;
 import com.owners.pet.petowners.models.User;
 
 import javax.annotation.Nullable;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private String uid;
     private String name;
+    private StorageReference storageRef;
+    private StorageReference chatUserProfileImageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        storageRef = FirebaseStorage.getInstance().getReference();
+
         Intent intent = getIntent();
         if(intent != null){
             uid = intent.getStringExtra(getString(R.string.USER_PROFILE_UID));
             name = intent.getStringExtra(getString(R.string.USER_PROFILE_NAME));
+
+            chatUserProfileImageRef = storageRef.child(getString(R.string.COLLECTION_USERS))
+                    .child(uid).child("profile.jpg");
         }
 
         final ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle(name);
+            actionBar.setDisplayShowCustomEnabled(true);
+
+            LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View custom_bar_view = inflater.inflate(R.layout.custom_chat_actionbar, null);
+
+            TextView chatUserNameTv = custom_bar_view.findViewById(R.id.chat_user_name);
+            CircleImageView chatUserProfileImageIv = custom_bar_view.findViewById(R.id.chat_profile_image);
+
+            chatUserNameTv.setText(name);
+            GlideApp.with(getApplicationContext())
+                    .load(chatUserProfileImageRef)
+                    .placeholder(R.drawable.profile_icon)
+                    .into(chatUserProfileImageIv);
+
+            actionBar.setCustomView(custom_bar_view);
         }
 
-
-        db = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
 
     }
 
