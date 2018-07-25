da40056aaa2ff586475ab1071d61ac5bf488794d
didem <kucukkaraaslan.didem@gmail.com>
2018-07-25 14:34:56 +0300

    glide removed

--- app/src/main/java/com/owners/pet/petowners/adapters/CustomInfoViewAdapter.java
+++ app/src/main/java/com/owners/pet/petowners/adapters/CustomInfoViewAdapter.java
@@ -1,26 +1,19 @@
 package com.owners.pet.petowners.adapters;
 
-import android.app.Activity;
+
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.owners.pet.petowners.Glide.GlideApp;
-import com.owners.pet.petowners.OthersProfileActivity;
-import com.owners.pet.petowners.ProfileActivity;
 import com.owners.pet.petowners.R;
-import com.owners.pet.petowners.models.User;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
@@ -30,7 +23,6 @@ public class CustomInfoViewAdapter implements GoogleMap.InfoWindowAdapter {
 
     private Context context;
     private StorageReference storageRef;
-    private StorageReference userProfileImageRef;
 
     public CustomInfoViewAdapter(Context context) {
         this.context = context;
@@ -42,7 +34,7 @@ public class CustomInfoViewAdapter implements GoogleMap.InfoWindowAdapter {
 
         View view = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null);
 
-        CircleImageView infoWindowProfileImage = view.findViewById(R.id.info_window_profile_image);
+        final CircleImageView infoWindowProfileImage = view.findViewById(R.id.info_window_profile_image);
         TextView infoWindowTitle = (TextView) view.findViewById(R.id.info_window_title);
         TextView infoWindowBody = (TextView) view.findViewById(R.id.info_window_body);
         TextView infoWindowINFO= (TextView) view.findViewById(R.id.info_window_tv);
@@ -56,14 +48,18 @@ public class CustomInfoViewAdapter implements GoogleMap.InfoWindowAdapter {
             String uid = tag.get(context.getString(R.string.UID_KEY));
             String color = tag.get(context.getString(R.string.COLOR_KEY));
 
-            userProfileImageRef = storageRef.child(context.getString(R.string.COLLECTION_USERS))
+            StorageReference userProfileImageRef = storageRef.child(context.getString(R.string.COLLECTION_USERS))
                     .child(uid).child("profile.jpg");
 
-            GlideApp.with(context)
-                    .load(userProfileImageRef)
-                    .placeholder(R.drawable.profile_icon)
-                    .into(infoWindowProfileImage);
-
+            userProfileImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.get()
+                            .load(uri)
+                            .placeholder(R.drawable.profile_icon)
+                            .into(infoWindowProfileImage);
+                }
+            });
 
 
             switch (color) {
