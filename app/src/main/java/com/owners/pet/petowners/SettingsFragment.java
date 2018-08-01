da1b742ea9f1adcc48f83134fbaacccc07e67745
didem <kucukkaraaslan.didem@gmail.com>
2018-08-01 22:32:30 +0300

    list preference setting summary

--- app/src/main/java/com/owners/pet/petowners/SettingsFragment.java
+++ app/src/main/java/com/owners/pet/petowners/SettingsFragment.java
@@ -1,14 +1,24 @@
 package com.owners.pet.petowners;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat  {
+    public static final String TAG = SettingsFragment.class.getSimpleName();
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_settings);
 
     }
+
+
 }
